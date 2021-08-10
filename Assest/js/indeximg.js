d0dba7792d04cf7a4db3ea4c0ca171acd252cccc
harikrishnan <hariece601@gmail.com>
2021-08-10 22:01:32 +0530

    added home page images

--- Assest/js/indeximg.js
+++ Assest/js/indeximg.js
@@ -1,10 +1,10 @@
-var kings=[{imageurl:"pexelsdairy12.jpg",name:"Birthday"},{imageurl:"anniversary min.png",name:"Annivesary"},{imageurl:"for her.png",name:"lovable one"},{imageurl:"3333.jpg",name:"for your lover" }];
+var kings=[{imageurl:"pexelsdairy12.jpg",name:"Birthday"},{imageurl:"anniversary min.png",name:"Annivesary"},{imageurl:"for her.png",name:"lovable one"},{imageurl:"love3.png",name:"for your lover" }];
 let content="";
 for(let king of kings)
 {
     content=content+`
     <div class="items1" >
-                <img src="images/${king.imageurl}" alt="img"><br>
+               <a href="occasions.html"> <img src="images/${king.imageurl}" alt="img"></a><br>
                 <p> ${king.name}</p>
                 
             </div>`;
