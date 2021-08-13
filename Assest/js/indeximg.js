fca30aa363bdc9c97285b64086ff1e346b0a2c9d
harikrishnan <hariece601@gmail.com>
2021-08-13 23:44:21 +0530

    added ordernow

--- Assest/js/indeximg.js
+++ Assest/js/indeximg.js
@@ -1,13 +1,13 @@
-var kings=[{imageurl:"pexelsdairy12.jpg",name:"Birthday"},
-{imageurl:"anniversary min.png",name:"Anniversary"},
-{imageurl:"for her.png",name:"Wedding Cake"},
-{imageurl:"love3.png",name:"For your lover" }];
+var kings=[{imageUrl:"pexelsdairy12.jpg",name:"Birthday"},
+{imageUrl:"anniversary min.png",name:"Anniversary"},
+{imageUrl:"for her.png",name:"Wedding Cake"},
+{imageUrl:"love3.png",name:"Lovable One" }];
 let content="";
 for(let king of kings)
 {
     content=content+`
     <div class="items1" >
-               <a href="occasions.html?category=${king.name}"> <img src="images/${king.imageurl}" alt="img"></a><br>
+               <a href="occasions.html?category=${king.name}"> <img src="images/${king.imageUrl}" alt="img"></a><br>
                 <p> ${king.name}</p>
                 
             </div>`;
