c8f0d2143d71d3f56fca63acda9de6e8c14ecd9b
harikrishnan <hariece601@gmail.com>
2021-08-13 19:24:57 +0530

    added ordernow page validation

--- Assest/js/ordernow.js
+++ Assest/js/ordernow.js
@@ -1,19 +1,43 @@
 function ordernow()
 {
+    alert("hello");
     event.preventDefault();
     const name=document.querySelector("#name").value;
-    const phonenumber=document.querySelector("#phoneNumber").value;
-    const date=document.querySelector("#date").value;
-    const time=document.querySelector("#time").value;
-    const fondant=document.querySelector("#fondant").value;
-    const icecake=document.querySelector("#icecake").value;
-    const buttercream=document.querySelector("#buttercake").value;
-    const cakeFlavour=document.querySelector("#cakeflavour").value;
-    const numberfkg=document.querySelector("#numberofkg").value;
-    const specialinstruction=document.querySelector("#specialinstruction").value;
-    const cakemessage=document.querySelector("#cakemessage").value;
+     const totalAmount=document.querySelector("#totalAmount").value;
+    const orderDate=document.querySelector("#orderDate").value;
+    const status=document.querySelector("#status").value;
     const address=document.querySelector("#address").value;
-    console.log(name+"-"+phoneNumber+"-"+date+"-"+time+"-"+fondant+"-"+icecake+"-"+buttercake+"-"+numberofkg+"-"+specialinstruction+"-"+cakemessage+"-"+address);
+    const comments=document.querySelector("#comments").value;
+    const createdDate=document.querySelector("#createdDate").value;
+    const modifiedDate=document.querySelector("#modifiedDate").value;
+    // const deliverdDate=document.querySelector("#deliveryDate").value;
+    const cancelledDate=document.querySelector("#cancelledDate").value;
+    // const cakemessage=document.querySelector("#cakemessage").value;
+    // const address=document.querySelector("#address").value;
+    // console.log(name+"-"+phoneNumber+"-"+date+"-"+time+"-"+fondant+"-"+icecake+"-"+buttercake+"-"+numberofkg+"-"+specialinstruction+"-"+cakemessage+"-"+address);
+
+    orderObj={
+        id:1,
+        applicationName:name,
+        totalAmount:totalAmount,
+        orderedDate:orderDate,
+        status:status,
+        address:address,
+        comments:comments,
+        createdDate:createdDate,
+        modifiedDate:modifiedDate,
+        deliverdDate:"hel",
+        cancelledDate:cancelledDate
+
+
+    };
+const url="https://product-mock-api.herokuapp.com/orderapp/api/v1/orders?applicationName=giftshop";
+alert("helo");
+axios.post(url,orderObj).then(res =>{
     alert("your order successfully placed");
     window.location.href="index.html";
+}).catch(err=>{
+    alert("order failed");
+});
+    
 }
