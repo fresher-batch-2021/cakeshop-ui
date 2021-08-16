0ad859a89c8b618065e2dfa40f001d762a070e9f
harikrishnan <hariece601@gmail.com>
2021-08-17 00:07:57 +0530

    added url for ordernow

--- Assest/js/ordernow.js
+++ Assest/js/ordernow.js
@@ -2,39 +2,35 @@ function ordernow()
 {
     event.preventDefault();
     const name=document.querySelector("#name").value;
-     const totalAmount=document.querySelector("#totalAmount").value;
-    const orderDate=document.querySelector("#orderDate").value;
-    const status=document.querySelector("#status").value;
+    const number=document.querySelector("#number").value;
+     const date=document.querySelector("#date").value; 
     const address=document.querySelector("#address").value;
-    const comments=document.querySelector("#comments").value;
-    const createdDate=document.querySelector("#createdDate").value;
-    const modifiedDate=document.querySelector("#modifiedDate").value;
-    const deliverdDate=document.querySelector("#deliveryDate").value;
-    const cancelledDate=document.querySelector("#cancelledDate").value;
+    const totalAmount=document.querySelector("#totalAmount").value;
+    
     
 
     orderObj={
-        id:1,
-        applicationName:name,
-        totalAmount:totalAmount,
-        orderedDate:orderDate,
-        status:status,
-        address:address,
-        comments:comments,
-        createdDate:createdDate,
-        modifiedDate:modifiedDate,
-        deliverdDate:"hel",
-        cancelledDate:cancelledDate
-
-
+         Name:name,
+         mobileNo:number,
+         date:date,
+         address:address,
+        totalAmount:totalAmount
     };
-const url="https://product-mock-api.herokuapp.com/orderapp/api/v1/orders?applicationName=giftshop";
+    const dbUsername = 'apikey-v2-237a9fx60g51gyopiewwx5pb339t2r1xw085fzt3skgx';
+    const dbPassword = '85e4a7e36372ac1e47c80f4b81a78d62';
+    const basicAuth = "Basic " +btoa(dbUsername+":"+dbPassword);
+const url="https://99560248-15e7-4158-bfde-3c13e3ebf4e9-bluemix.cloudantnosqldb.appdomain.cloud/cakeshop_ordernow";
 alert("helo");
-axios.post(url,orderObj).then(res =>{
+
+axios.post(url,orderObj,{headers:{'Authorication':basicAuth}}).then(res =>{
+    const data = res.data.rows;
+    const products = data.map(obj=>obj.doc);
+    console.log(products);
     alert("your order successfully placed");
     window.location.href="index.html";
 }).catch(err=>{
-    alert("order failed");
+    console.log(err.response.data);
+    alert(" Your Order is Failed ");
 });
     
 }
--- Assest/js/register.js
+++ Assest/js/register.js
@@ -57,7 +57,7 @@ function registerAPI(regObj){
     const dbUsername = 'apikey-v2-237a9fx60g51gyopiewwx5pb339t2r1xw085fzt3skgx';
     const dbPassword = '85e4a7e36372ac1e47c80f4b81a78d62';
     const basicAuth = "Basic " + btoa(dbUsername + ":" + dbPassword);
-    const url = "https://99560248-15e7-4158-bfde-3c13e3ebf4e9-bluemix.cloudantnosqldb.appdomain.cloud/cakeshop";
+    const url = "https://99560248-15e7-4158-bfde-3c13e3ebf4e9-bluemix.cloudantnosqldb.appdomain.cloud/cakeshop_user";
     axios.post(url, regObj, { headers: { Authorization: basicAuth } }).then(res => {
         let data = res.data;
         console.log(data);
